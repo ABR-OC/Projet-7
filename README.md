2ce6d720b7328d9c1b28d127acc11fd2aab3a06e
Adrien Brugère <63599254+ABR-OC@users.noreply.github.com>
2020-09-17 11:05:37 +0200

    Ini P7 - First commit - frontend

---
+++ README.md
@@ -0,0 +1,33 @@
+# Projet-7
+Créez un réseau social d’entreprise : Groupomania
+
+<b>Technologies utilisées :</b>
+
+● serveur : NodeJS ;<br />
+● framework : Express ;<br />
+● base de données : MySQL ;<br />
+
+<b>Procédure :</b>
+
+● Backend :
+
+Créer un base de donnée mysql
+
+Dans le dossier "config" fichier "db.js" rentrer les identifiants de votre base :
+
+    Ex : "const sequelize = new Sequelize(NOM_DE_LA_BD, USERNAME, PASSWORD, {"
+
+Installer les dépendances: "npm install"
+
+Lancer: "nodemon server"
+
+● Frontend :
+
+Installer les dépendances: "npm install"
+
+Dans le dossier "frontend" lancez le fichier "index.html"
+
+
+
+
+
---
+++ frontend/js/login.js
@@ -0,0 +1,40 @@
+
+const email = document.getElementById('email')
+const password = document.getElementById('psw')
+const errorMessage = document.getElementById('error-message')
+const btn = document.getElementById('btn')
+
+const url = 'http://localhost:4000/api/auth/login'
+
+
+/* Envoie les données à l'api */
+const postData = async (url, dataElt) => {
+    const response = await fetch(url, {
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(dataElt)
+    })
+    return await response.json();
+}
+
+btn.addEventListener("click", async (e) => {
+    e.preventDefault(); 
+    const login = {
+        email: email.value,
+        password: password.value
+    }
+        const data = await postData(url, login); // Envoie les données au serveur 
+        if ( data.error ) {
+            errorMessage.textContent = data.error;
+            return console.error(data.error)
+        }
+        sessionStorage.setItem('status', ('connected')) ; 
+        sessionStorage.setItem('token', `${data.token}`) 
+        window.location = `index.html`; // Redirige vers la page d'accueil
+})
+
+if( sessionStorage.getItem('status') && window.location.href.includes('login.html')) {
+    window.location.replace('index.html')  
+}
